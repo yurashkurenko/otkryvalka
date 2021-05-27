9c76b0b3c1ed20f26cbc6d5e5bc120b649679dd5
yurashkurenko <yurashkurenko@users.noreply.github.com>
2021-05-27 13:00:22 +0300

    Add files via upload

--- bot26052021/edituserboxes.py
+++ bot26052021/edituserboxes.py
@@ -15,14 +15,25 @@ def append(user,box):
     return 
 
 def remove(user,box):
-    boxes = []
-    boxes=pdata.read(boxes,'boxes')
-    print(boxes)
-    if boxes.count(box)>0:
-        boxes.remove(box)
-        pdata.write(boxes,'boxes')
-        print(boxes)
+    userboxes = []
+    userboxes=pdata.read(userboxes,'userboxes')
+    print(userboxes)
+    for i in range(len(userboxes)):
+        if userboxes[i][0]==user:
+            if userboxes[i].count(box)>0:
+                userboxes[i].remove(box)
+    print(userboxes)
     return 
+
+def removeuser(user):
+    userboxes = []
+    userboxes=pdata.read(userboxes,'userboxes')
+    print(userboxes)
+    for i in range(len(userboxes)):
+        if userboxes[i][0]==user:
+            userboxes.remove([user])
+    print(userboxes)
+    return
 #    users.remove(x)
 #    return datap
 #list.count(x)	Возвращает количество элементов со значением x
