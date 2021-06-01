cef44b1d22ecb22dbfaa1f6c1645e8aa5bc81ec5
yurashkurenko <yurashkurenko@users.noreply.github.com>
2021-06-01 16:49:52 +0300

    Add files via upload

--- botdb/exampledb.py
+++ botdb/exampledb.py
@@ -1,16 +1,75 @@
 import db
 # rows={}
-rows= {'user': 'yurasoft1'}
-table='users'
-db.insert(table,rows)
-rows= {'user': 'yurasoft1-'}
-table='users'
-db.insert(table,rows)
-rows= {'user': 'yurasoft1+'}
-table='users'
-db.insert(table,rows)
+# rows= {'user': 'yurasoft1'}
+# table='users'
+# db.insert(table,rows)
+# rows= {'user': 'yurasoft1-'}
+# table='users'
+# db.insert(table,rows)
+# rows= {'user': 'yurasoft1+'}
+# table='users'
+# db.insert(table,rows)
 
-rowsfetch=['id','user']
-print(db.fetchall(table,rowsfetch))
-rowsfetch=['id']
-print(db.fetchall(table,rowsfetch))
+# rowsfetch=['id','user']
+# print(db.fetchall(table,rowsfetch))
+# rowsfetch=['id']
+# rowsfetch=['user']
+# print(db.fetchall(table,rowsfetch))
+
+#users={}
+#users=db.fetchall('users',['user'])
+
+#print(users)
+#['user']
+# # [{'user': 'yurasoft10'}, {'user': 'yurasoft10'}, {'user': 'yurasoft10'}, {'user': 'yurasoft10'}, {'user': 'yurasoft10'}, {'user': 'yurasoft10'}, {'user': 'yurasoft1'}, {'user': 'yurasoft1-'}, {'user': 'yurasoft1+'}, #{'user': 'yurasoft1'}, {'user': 'yurasoft1-'}, {'user': 'yurasoft1+'}, {'user': 'yurasoft1'}, {'user': 'yurasoft1-'}, {'user': 'yurasoft1+'}, {'user': 'yurasoft1'}, {'user': 'yurasoft1-'}, {'user': 'yurasoft1+'}, {'user': # #'yurasoft1'}, {'user': 'yurasoft1-'}, {'user': 'yurasoft1+'}]
+#username=users[0]
+# список словарей
+
+#print(list(username.values())[0])
+# выводит yurasoft10
+
+
+# list юзеров
+# users=[]
+# users=db.fetchall('users',['user'])
+# for j in range(len(users)):
+    # print(list(users[j].values())[0])
+
+# yurasoft10
+# yurasoft10
+# yurasoft10
+# yurasoft10
+# yurasoft10
+# yurasoft10
+# yurasoft1
+# yurasoft1-
+# yurasoft1+
+# yurasoft1
+# yurasoft1-
+# yurasoft1+
+# yurasoft1
+# yurasoft1-
+# yurasoft1+
+# yurasoft1
+# yurasoft1-
+# yurasoft1+
+# yurasoft1
+# yurasoft1-
+# yurasoft1+
+
+#print[username['user']]
+
+
+# list боксов
+# boxes=[]
+# boxes=db.fetchall('boxes',['name'])
+# for j in range(len(boxes)):
+    # print(list(boxes[j].values())[0])
+    
+    
+# C:\install\botdb>py C:\install\botdb\exampledb.py
+# 1.2
+# 1.1
+# 11
+# 123
+
