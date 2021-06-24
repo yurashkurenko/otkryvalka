60adbc8edb7b0f4bd01f8d35c2cffda00ab7f0b3
yurashkurenko <yurashkurenko@users.noreply.github.com>
2021-06-24 22:06:28 +0300

    Update stat.py

--- qipy/stat.py
+++ qipy/stat.py
@@ -8,8 +8,8 @@ def payment_history_summ_dates(my_login, api_access_token, start_Date, end_Date)
     h = s.get('https://edge.qiwi.com/payment-history/v2/persons/' + my_login + '/payments/total', params = parameters)
     return h.json()
 
-my_login='79054725000'
-api_access_token='3288c8b8da086a3cd654851dbfc0c057'
+my_login=''
+api_access_token=''
 start_Date='2021-05-01T12:53:30+03:00'
 end_Date='2021-05-31T12:53:30+03:00'
 #start_Date='2021-03-01T00%3A00%3A00%2B03%3A00'
