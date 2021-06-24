63672edd39d2a8ecb1b67169487d98b96c1d933b
yurashkurenko <yurashkurenko@users.noreply.github.com>
2021-06-24 22:05:27 +0300

    Update qipy.py

--- qipy/qipy.py
+++ qipy/qipy.py
@@ -1,5 +1,5 @@
 import requests
-pload = {"Accept":"application/json","Content-Type":"application/json","Authorization":"Bearer 3288c8b8da086a3cd654851dbfc0c057"}
+pload = {"Accept":"application/json","Content-Type":"application/json","Authorization":"Bearer "}
 url="https://edge.qiwi.com/payment-history/v2/persons/79054725000/payments?rows=50"
 r = requests.get(url,headers=pload)
 
